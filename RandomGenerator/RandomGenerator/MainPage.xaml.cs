0234b4008e2331178a08d6473d191c81fd6ad97c
DJ <deejcarpenter9@yahoo.com>
2021-09-26 15:12:49 -0400

    MinViableProduct

--- RandomGenerator/RandomGenerator/MainPage.xaml.cs
+++ RandomGenerator/RandomGenerator/MainPage.xaml.cs
@@ -34,20 +34,28 @@ namespace RandomGenerator
             classMembers[12] = "Torrie";
             classMembers[13] = "Yohan";
             //binds ^ above array list to the listview display
-            x:classMemberList.ItemsSource = classMembers;
+            classMemberList.ItemsSource = classMembers;
 
             Random rand = new Random();
             int index = rand.Next(classMembers.Length);
             return classMembers[index];
-            
+            //printRandomName.Text = classMembers[index];
         }
+       
 
         private void btnRandomize_Clicked(object sender, EventArgs e)
         {
             //TODO figure out how to print random name to screen
             //TODO ClassMembers() as called in the public MainPage adds classMembers to listview (i think) and also randomizes and returns something. It should only do the first part (add classMembers)
             //...the randomization and printing should be done somewhere else (i think)
+            //Random rand = new Random();
+            //int index = rand.Next(ClassMembers().Length);
+            //rand.Next(ClassMembers().Length);
+            //ClassMembers randClass = new ClassMembers(string);
 
+            printRandomName.Text = ClassMembers();
+            printRandomName.FontSize = 50;
+            printRandomName.TextColor = Color.Red;
         }
         
     }
