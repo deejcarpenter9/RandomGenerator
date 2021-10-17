893103a983f4bf0076e08d4dcd9bf535b8f3acd6
DJ <deejcarpenter9@yahoo.com>
2021-10-16 21:38:10 -0400

    Made separate methods instead of having everything built into 1 method. Deleted some code I had on here as an experiment for another project.

--- RandomGenerator/RandomGenerator/MainPage.xaml.cs
+++ RandomGenerator/RandomGenerator/MainPage.xaml.cs
@@ -13,36 +13,31 @@ namespace RandomGenerator
         public MainPage()
         {
             InitializeComponent();
-            ClassMembers();
+            ClassMembers(); //Invokes Classmembers method when MainPage is displayed.
         }
 
-        
-        public string ClassMembers()
-        {
-            string[] classMembers = new string[14] { "Billy", "Dennis", "Josh", "Lumiere", "Michael", "Joe", "Jay", "Austin", "Dan", "Erick", "Wynn", "Scott", "Torrie", "Yohan" };
-            //binds ^ above array list to the listview display
-            classMemberList.ItemsSource = classMembers;
+        //declares array classMembers to be listed via ClassMembers() method 
+        public string[] classMembers = new string[14] { "Billy", "Dennis", "Josh", "Lumiere", "Michael", "Joe", "Jay", "Austin", "Dan", "Erick", "Wynn", "Scott", "Torrie", "Yohan" };
 
+        public void ClassMembers()
+        {
+            classMemberList.ItemsSource = classMembers; //Assigns classMembers Array to classMemberList Listview on MainPage.xaml
+        }
+       
+        //When Randomize button is clicked, changes label named printRandomName
+        private void btnRandomize_Clicked(object sender, EventArgs e)
+        {
+            printRandomName.Text = Randomize();
+            printRandomName.FontSize = 50;
+            printRandomName.TextColor = Color.Red;
+        }
+
+        //Performs randomizing upon button click
+        private string Randomize()
+        {
             Random rand = new Random();
             int index = rand.Next(classMembers.Length);
             return classMembers[index];
-            
-        }
-       
-
-        private void btnRandomize_Clicked(object sender, EventArgs e)
-        {
-            //TODO figure out how to print random name to screen
-            //TODO ClassMembers() as called in the public MainPage adds classMembers to listview (i think) and also randomizes and returns something. It should only do the first part (add classMembers)
-            //...the randomization and printing should be done somewhere else (i think)
-            //Random rand = new Random();
-            //int index = rand.Next(ClassMembers().Length);
-            //rand.Next(ClassMembers().Length);
-            //ClassMembers randClass = new ClassMembers(string);
-
-            printRandomName.Text = ClassMembers();
-            printRandomName.FontSize = 50;
-            printRandomName.TextColor = Color.Red;
         }
         
     }
